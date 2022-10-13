ed71c4bd6521506f1090a70a49643b4e32995591
skrjtech <skrjtech@gmail.com>
2022-10-13 14:30:43 +0000

    __init__

---
+++ mydeepface/mydeepface/models/__init__.py
@@ -0,0 +1,3 @@
+from mydeepface.mydeepface.models.predictor import Predictor
+from mydeepface.mydeepface.models.generator import Generator
+from mydeepface.mydeepface.models.discriminator import Discriminator
