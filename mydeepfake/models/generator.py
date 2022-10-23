cc6cd53eaa0f2073ba46e571872be70fda0d2dd3
skrjtech@skrtech.com <skrjtech>
2022-10-23 17:12:50 +0000

    generator

--- mydeepfake/models/generator.py
+++ mydeepfake/models/generator.py
@@ -2,9 +2,10 @@ import torch
 from mydeepfake.models.modelbase import ResidualBlock
 
 class Generator(torch.nn.Module):
+    count: int = 0
     def __init__(self, input_nc, output_nc, n_residual_blocks=9):
         super(Generator, self).__init__()
-
+        self.count = Generator.count = Generator.count + 1
         self.model = torch.nn.Sequential(
             torch.nn.ReflectionPad2d(3),
             torch.nn.Conv2d(input_nc, 64, 7),
