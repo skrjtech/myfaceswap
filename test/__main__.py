0a360b3da388b69a029e885e93b97a0902ec5790
skrjtech@skrtech.com <skrjtech>
2022-10-23 17:13:03 +0000

    __main__

--- test/__main__.py
+++ test/__main__.py
@@ -1,3 +1,14 @@
 if __name__ == '__main__':
-    import mydeepfake
-    print(mydeepfake.__name__)
+    class Obj():
+        count: int = 0
+        def __init__(self):
+            Obj.count += 1
+            self.count = Obj.count
+    O1 = Obj()
+    O2 = Obj()
+    O3 = Obj()
+    print(O1.count)
+    print(O2.count)
+    print(O3.count)
+    print(O1.count)
+    print(O3.__class__.__name__)
