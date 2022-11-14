58e6d397afa2a8a4087a94c51c45b85afd4c1cc8
skrjtech <skrjtech>
2022-11-14 16:45:33 +0000

    __init__

--- mydeepfake/dataset/datasetsquence.py
+++ mydeepfake/dataset/datasetsquence.py
@@ -1,4 +1,3 @@
-from optparse import Option
 import os
 import glob
 import random
@@ -12,25 +11,42 @@ import torchvision.transforms
 from PIL import Image
 
 class FaceDatasetSquence(torch.utils.data.Dataset):
-    def __init__(self, path: str, transforms: Optional[List]=None, unaligned: bool=False, mode: str='train', domainA: str='fadg0/video/', domainB: str='faks0/video', skip: int=0) -> None:
-        super().__init__()
+    def __init__(
+        self, 
+        path: str, 
+        transforms: Optional[List]=None, 
+        unaligned: bool=False, 
+        mode: str='train', 
+        domainA: str='fadg0/video/', 
+        domainB: str='faks0/video', 
+        skip: int=0) -> None:
+        """
+        :param path: str
+        :param transforms: list
+        :param unaligned: bool
+        :param mode: str
+        :param domainA: str
+        :param domainB: str
+        :param skip: int
+        """
+        super(FaceDatasetSquence, self).__init__()
         self.transform = torchvision.transforms.Compose(transforms)
         self.unaligned = unaligned
         self.mode = mode
         self.skip = skip
-        directoryA = sorted(os.listdir(os.path.join(path, domainA)))
-        directoryB = sorted(os.listdir(os.path.join(path, domainB)))
+        directoryAB = list(map(lambda x: sorted(os.listdir(os.path.join(path, "dataset", x))), [domainA, domainB]))
         self.filterListA = []
         self.filterListB = []
         self.remove_num = remove_num = (skip + 1) * 2
-        for dirA, dirB, in zip(directoryA, directoryB):
-            _A = sorted(glob.glob(os.path.join(path, domainA, dirA + '/*')))
-            _B = sorted(glob.glob(os.path.join(path, domainB, dirB + '/*')))
-            self.filterListA += _A[: -remove_num]
-            self.filterListB += _B[: -remove_num]
-        self.filterListANum = len(self.filterListA)
-        self.filterListBNum = len(self.filterListB)
-        
+        def dirTenkai(directory, domain):
+            res = []
+            for dir in directory:
+                A = sorted(glob.glob(os.path.join(path, domain, dir + '/*')))
+                res += A[: -remove_num]
+            return res
+        self.filterListAB = list(map(lambda x: dirTenkai(*x), [[directoryAB[0], domainA], [directoryAB[1], domainB]]))
+        self.filterListANum = len(self.filterListAB[0])
+        self.filterListBNum = len(self.filterListAB[1])
         self.count = 1234
     def __getitem__(self, idx):
         seed = self.count
@@ -42,19 +58,17 @@ class FaceDatasetSquence(torch.utils.data.Dataset):
             fileNum = fileNum.split('.')[0]
             file2 = os.path.join(dirName, '{:0=5}.png'.format(int(fileNum) + self.skip))
             file3 = os.path.join(dirName, '{:0=5}.png'.format(int(fileNum) + self.skip * 2))
-            return Transform(file1), Transform(file2), Transform(file3)
-            
-        fileA1 = self.filterListA[idx % self.filterListANum]
+            return tuple(map(Transform, [file1, file2, file3]))
+        fileA1 = self.filterListAB[0][idx % self.filterListANum]
         itemA1, itemA2, itemA3 = SequenceData(fileA1)
         if self.unaligned: fileB1 = self.filterListB[random.randint(0, self.filterListBNum - 1)]
-        else: fileB1 = self.filterListB[idx % self.filterListBNum]
+        else: fileB1 = self.filterListAB[1][idx % self.filterListBNum]
         itemB1, itemB2, itemB3 = SequenceData(fileB1, seed=seed+1)
         self.count += 1
         return {
             "A1": itemA1, "A2": itemA2, "A3": itemA3,
             "B1": itemB1, "B2": itemB2, "B3": itemB3
         }
-    
     def __len__(self):
         return max(self.filterListANum, self.filterListBNum)
 
@@ -88,3 +102,9 @@ class FaceDatasetVideo(torch.utils.data.Dataset):
         
     def __len__(self):
         return len(self.files)
+
+if __name__ == "__main__":
+    dataset = FaceDatasetSquence("/ws/ioRoot", unaligned=True, domainA="domainA", domainB="domainB", skip=2)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=8)
+    batch = next(iter(dataloader))
+    print(batch)
